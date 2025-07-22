cbbdc7755dc9c9e8e1cff61dc0ad1ead9aa68f4d
周睿 <zrufo747@outlook.com>
2025-07-22 17:48:27 +0800

    add test

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cargo test --target aarch64-unknown-none-softfloat -p test-base --test test --features qemu -- --show-output
+cargo test --target aarch64-unknown-none-softfloat -p test-base --test test  -- --show-output
