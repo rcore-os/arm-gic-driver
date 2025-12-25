146ce510ac2d726121f5b870c4db62ca6bc5a69c
WEIXIAOYVYI <42529620+WEIXIAOYVYI@users.noreply.github.com>
2025-12-25 12:04:29 +0800

    Fix SGI/PPI priority initialization for GICv2 on secondary CPUs (#13)
    * fix: set sgi and ppi default priorities by cpu
    
    Signed-off-by: luozhixian <luozhixian@kylinos.cn>
    
    * rename num_regs to num_interrupts
    
    Signed-off-by: luozhixian <luozhixian@kylinos.cn>
    
    * add some comment
    
    Signed-off-by: luozhixian <luozhixian@kylinos.cn>
    
    ---------
    
    Signed-off-by: luozhixian <luozhixian@kylinos.cn>
    Co-authored-by: luozhixian <luozhixian@kylinos.cn>

--- gic-driver/src/version/v2/mod.rs
+++ gic-driver/src/version/v2/mod.rs
@@ -106,8 +106,8 @@ impl Gic {
         self.gicd().groups_all_to_0(max_spi);
         trace!("[GICv2] Configure all interrupts as Group 1 (Non-secure) by default");
 
-        // 7. Set default priority for all interrupts
-        self.gicd().set_default_priorities(max_spi);
+        // 7. Set default priority for spi interrupts
+        self.gicd().set_default_spi_priorities(max_spi);
 
         // 8. Configure interrupt targets (for SPIs)
         self.gicd().configure_interrupt_targets(max_spi);
@@ -376,6 +376,9 @@ impl CpuInterface {
 
         // 5. Enable CPU interface for both Group 0 and Group 1 interrupts
         gicc.CTLR.write(gicc::CTLR::EnableGrp0::SET);
+
+        // 6. Set default priority for sgi and ppi interrupts
+        self.gicd().set_default_sgi_ppi_priorities();
     }
     /// Set the EOI mode for non-secure interrupts
     ///
